939d106e9063305d878c3b5905e3c98399d63429
VarunHari12 <112435895+VarunHari12@users.noreply.github.com>
2022-09-11 16:18:48 -0700

    added comments to the code and adjusted some minor bugs

--- main.py
+++ main.py
@@ -1,68 +1,103 @@
-obstacles: List[game.LedSprite] = []  # list of "game.LedSprite" objects
+obstacles: List[game.LedSprite] = []  # list of "game.LedSprite" objects that are obstacles
 bird: game.LedSprite = game.create_sprite(0, 0) # bird sprite
-score = 0
-ticks = 0 # we increment this on every loop iteration
 
-def on_button_pressed_a():
-    bird.change(LedSpriteProperty.Y, 1)
+# these are the handlers for the events for the buttons and gesture
+# they check to see if certain buttons are pressed and if so they call certain functions
+
+# input handler setup for button A
 input.on_button_pressed(Button.A, on_button_pressed_a)
-
-def on_button_pressed_b():      # up and down
-    bird.change(LedSpriteProperty.Y, -1)
+# input handler setup for button B
 input.on_button_pressed(Button.B, on_button_pressed_b)
+# input handler setup for button A + B
+input.on_button_pressed(Button.AB, on_button_pressed_ab)
+# input handler setup for gesture SHAKE
+#input.on_gesture(Gesture.SHAKE, on_gesture_shake)
 
 
-#def on_button_pressed_ab(): # pause
-#    game.pause()
-#input.on_button_pressed(Button.B, on_button_pressed_ab)
 
-def initialize():
+# basic.forever keeps looping while also allowing other things to happen at the same time
+basic.forever(on_forever)
+
+# this method is a handler that checks if the user shakes the microbit
+# if there is a shake detected it pauses/unpauses the game
+# def on_gesture_shake():
+    # if game.is_paused():
+         # game.resume()
+    # if game.is_running():
+        # game.pause()
+
+# this method is a handler that checks if the a button was pressed
+# if so it moves the bird sprite up one LED direction
+def on_button_pressed_a():
+    bird.change(LedSpriteProperty.Y, -1)
+
+# this method is a handler that checks if the b button was pressed
+# if so it moves the bird sprite down one LED in the y direction
+def on_button_pressed_b():      
+    bird.change(LedSpriteProperty.Y, 1)
+
+# this method is a handler that checks if the a and b button was pressed at the same time
+# if so it restarts the game
+def on_button_pressed_ab(): # pause
+    basic.forever(on_forever)
+
+# this method initializes all of the obstacles used in the game
+def InitializeObstacles():
     global obstacles
-    obstacles = []
-    gap = randint(0, 4)
-    for i in range(5):  # create 5 obstacles
+    obstacles = [] # creates an empty list of obstacles
+    gap = randint(0, 4) # picks a random y value between 0 and 4 for the gap to be in
+
+    # creates the lines of LEDs used for the obstacles 
+    # except for the index which is the gap in the obstacle
+    for i in range(5):  
             if i != gap:
-                obstacles.append(game.create_sprite(4, i))  # create a sprite at x=4, y=i
+                obstacle = game.create_sprite(4, i)
+                obstacle.turn(Direction.LEFT, 180) # sets obstacle movement to left
+                obstacles.append(obstacle)  
 
-    for i in range(obstacles.length):
-        obstacle = obstacles.get(i)
-        obstacle.turn(Direction.LEFT, 180)
 
+# this method moves each LED in the given obstacle one to the left then checks if game is over
+# if the game is not over, and the obstacles are on the edge, it will reset the obstacles in
+# the original position and get a new obstacle to use
 def moveleft():
     global obstacles
     for i in range(obstacles.length):
             obstacle = obstacles.get(i)
-            obstacle.move(1)
+            obstacle.move(1) # moves the obstacle one over in the direction of movement assigned
 
-    checkGameOver()
+    checkGameOver() # calls a method that checks if the game is over
 
-    onedge = False
+    # the following loop checks if the obstacles are on the edge
+    # if on the edge, it will set the onedge boolean to true and it clears the obstacle
+    onedge = False 
     for i in range(obstacles.length):
         obstacle = obstacles.get(i)
-        x = obstacle.get(LedSpriteProperty.X)
+        x = obstacle.get(LedSpriteProperty.X) # gets the obje
         if (x == 0):
             onedge = True
             obstacle.set(LedSpriteProperty.BRIGHTNESS, 0)
-    if (onedge):
-        initialize()
-        return       
-    
 
+    # if the obstacles are on the edge, reinitialize the obstacles from the start
+    if (onedge):
+        InitializeObstacles()
+        return
+    
+# this method checks if the games is over by checking if the bird is touching the obstacle
 def checkGameOver():
     global obstacles
-    for i in range(obstacles.length):
+    for i in range(obstacles.length): # goes through each obstacle that has been created
             obstacle = obstacles.get(i)
-            if bird.is_touching(obstacle):
+            if bird.is_touching(obstacle): # checks if the bird is touching the obstacle at index i
                 game.game_over()
 
-def on_forever():     # some microbit thing that lets the game state work in the background
-    global obstacles 
+# this method is a loop that reiterates the movement of the obstacles 
+def on_forever():     
+    global obstacles
 
     if obstacles.length <= 0:
-        initialize()
+        InitializeObstacles()
 
     moveleft()
 
     basic.pause(500) # pause for half a second, so the game doesn't run too fast
 
-basic.forever(on_forever)
--- main.ts
+++ main.ts
@@ -1,40 +1,63 @@
 let obstacles : game.LedSprite[] = []
-//  list of "game.LedSprite" objects
+//  list of "game.LedSprite" objects that are obstacles
 let bird = game.createSprite(0, 0)
 //  bird sprite
-let score = 0
-let ticks = 0
-//  we increment this on every loop iteration
+//  these are the handlers for the events for the buttons and gesture
+//  they check to see if certain buttons are pressed and if so they call certain functions
+//  input handler setup for button A
 input.onButtonPressed(Button.A, function on_button_pressed_a() {
-    bird.change(LedSpriteProperty.Y, 1)
-})
-input.onButtonPressed(Button.B, function on_button_pressed_b() {
-    //  up and down
     bird.change(LedSpriteProperty.Y, -1)
 })
-// def on_button_pressed_ab(): # pause
-//     game.pause()
-// input.on_button_pressed(Button.B, on_button_pressed_ab)
-function initialize() {
-    let i: number;
+//  input handler setup for button B
+input.onButtonPressed(Button.B, function on_button_pressed_b() {
+    bird.change(LedSpriteProperty.Y, 1)
+})
+//  input handler setup for button A + B
+input.onButtonPressed(Button.AB, function on_button_pressed_ab() {
+    //  pause
+    basic.forever(on_forever)
+})
+//  input handler setup for gesture SHAKE
+// input.on_gesture(Gesture.SHAKE, on_gesture_shake)
+//  basic.forever keeps looping while also allowing other things to happen at the same time
+basic.forever(on_forever)
+//  this method is a handler that checks if the user shakes the microbit
+//  if there is a shake detected it pauses/unpauses the game
+//  def on_gesture_shake():
+//  if game.is_paused():
+//  game.resume()
+//  if game.is_running():
+//  game.pause()
+//  this method is a handler that checks if the a button was pressed
+//  if so it moves the bird sprite up one LED direction
+//  this method is a handler that checks if the b button was pressed
+//  if so it moves the bird sprite down one LED in the y direction
+//  this method is a handler that checks if the a and b button was pressed at the same time
+//  if so it restarts the game
+//  this method initializes all of the obstacles used in the game
+function InitializeObstacles() {
     let obstacle: game.LedSprite;
     
     obstacles = []
+    //  creates an empty list of obstacles
     let gap = randint(0, 4)
-    for (i = 0; i < 5; i++) {
-        //  create 5 obstacles
+    //  picks a random y value between 0 and 4 for the gap to be in
+    //  creates the lines of LEDs used for the obstacles 
+    //  except for the index which is the gap in the obstacle
+    for (let i = 0; i < 5; i++) {
         if (i != gap) {
-            obstacles.push(game.createSprite(4, i))
+            obstacle = game.createSprite(4, i)
+            obstacle.turn(Direction.Left, 180)
+            //  sets obstacle movement to left
+            obstacles.push(obstacle)
         }
         
     }
-    //  create a sprite at x=4, y=i
-    for (i = 0; i < obstacles.length; i++) {
-        obstacle = obstacles.get(i)
-        obstacle.turn(Direction.Left, 180)
-    }
 }
 
+//  this method moves each LED in the given obstacle one to the left then checks if game is over
+//  if the game is not over, and the obstacles are on the edge, it will reset the obstacles in
+//  the original position and get a new obstacle to use
 function moveleft() {
     let i: number;
     let obstacle: game.LedSprite;
@@ -44,44 +67,53 @@ function moveleft() {
         obstacle = obstacles.get(i)
         obstacle.move(1)
     }
+    //  moves the obstacle one over in the direction of movement assigned
     checkGameOver()
+    //  calls a method that checks if the game is over
+    //  the following loop checks if the obstacles are on the edge
+    //  if on the edge, it will set the onedge boolean to true and it clears the obstacle
     let onedge = false
     for (i = 0; i < obstacles.length; i++) {
         obstacle = obstacles.get(i)
         x = obstacle.get(LedSpriteProperty.X)
+        //  gets the obje
         if (x == 0) {
             onedge = true
             obstacle.set(LedSpriteProperty.Brightness, 0)
         }
         
     }
+    //  if the obstacles are on the edge, reinitialize the obstacles from the start
     if (onedge) {
-        initialize()
+        InitializeObstacles()
         return
     }
     
 }
 
+//  this method checks if the games is over by checking if the bird is touching the obstacle
 function checkGameOver() {
     let obstacle: game.LedSprite;
     
     for (let i = 0; i < obstacles.length; i++) {
+        //  goes through each obstacle that has been created
         obstacle = obstacles.get(i)
         if (bird.isTouching(obstacle)) {
+            //  checks if the bird is touching the obstacle at index i
             game.gameOver()
         }
         
     }
 }
 
-//  pause for half a second, so the game doesn't run too fast
-basic.forever(function on_forever() {
-    //  some microbit thing that lets the game state work in the background
+//  this method is a loop that reiterates the movement of the obstacles 
+function on_forever() {
     
     if (obstacles.length <= 0) {
-        initialize()
+        InitializeObstacles()
     }
     
     moveleft()
     basic.pause(500)
-})
+}
+
